cd6cc604c5492815b6c4d1e7fd440aaf92b8646d
MarkHaoxiang <mark.haoxiang@gmail.com>
2025-04-21 00:17:48 +0100

    Parallize sweep

--- experiments/learn_voting_rules/sweep.sh
+++ experiments/learn_voting_rules/sweep.sh
@@ -1,5 +1,14 @@
-uv run python main.py hydra.mode=MULTIRUN \
-    'voting_rule=plurality,borda,copeland,minimax,stv' \
+#!/bin/bash
+
+VOTING_RULES=(plurality borda copeland minimax stv)
+
+for vr in "${VOTING_RULES[@]}"; do
+  echo "Launching job for voting_rule=$vr"
+  uv run python main.py hydra.mode=MULTIRUN \
+    'voting_rule=$vr' \
     'representation=graph,set' \
     'model_size=small,medium' \
-    '+repeat_number=range(10)'
+    '+repeat_number=range(10)' &
+done
+
+wait
